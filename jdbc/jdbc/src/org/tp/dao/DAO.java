7d62d6e5151fe73bf048bfaee8e626a49a5a0e80
olivier.pitton <olivier.pitton@kubity.com>
2015-02-09 14:38:16 +0100

    Add comments

--- jdbc/jdbc/src/org/tp/dao/DAO.java
+++ jdbc/jdbc/src/org/tp/dao/DAO.java
@@ -8,16 +8,50 @@ import java.util.List;
  */
 public interface DAO<T> {
 
+    /**
+     * Adds a new user and returns him with his new generated id.
+     * @param object an user
+     * @return a synchronized user with db
+     * @throws SQLException
+     */
     T add(T object) throws SQLException;
 
+    /**
+     * Remove the specified user from the DB
+     * @param object an user
+     * @throws SQLException
+     */
     void remove(T object)  throws SQLException;
 
+    /**
+     * Update the specified user. Returns the synchronized user with db
+     * @param object an user
+     * @return a synchronized user with db
+     * @throws SQLException
+     */
     T merge(T object) throws SQLException;
 
+    /**
+     * Retrieves an user based on his id
+     * @param id an user id
+     * @return a user
+     * @throws SQLException
+     */
     T find(long id) throws SQLException;
 
+    /**
+     * Retrieves an user based on his email
+     * @param name an email
+     * @return a user
+     * @throws SQLException
+     */
     T find(String name) throws SQLException;
 
+    /**
+     * Retrieves all users from the database
+     * @return list of all users
+     * @throws SQLException
+     */
     List<T> findAll() throws SQLException;
 
 }
--- jdbc/jdbc/src/org/tp/dao/UserDAO.java
+++ jdbc/jdbc/src/org/tp/dao/UserDAO.java
@@ -17,88 +17,33 @@ public class UserDAO implements DAO<User> {
         this.connection = connection;
     }
 
-
     @Override
     public User add(User object) throws SQLException {
-        try(PreparedStatement st = connection.prepareStatement("INSERT INTO users (email, password) VALUES(?, ?)", Statement.RETURN_GENERATED_KEYS)) {
-            st.setString(1, object.getMail());
-            st.setString(2, object.getPassword());
-            st.executeUpdate();
-            try(ResultSet rs = st.getGeneratedKeys()) {
-                if(rs.next()) {
-                    object.setId(rs.getLong(1));
-                }
-            }
-        }
-        return object;
+        return null;
     }
 
     @Override
     public void remove(User object) throws SQLException {
-        try(PreparedStatement st = connection.prepareStatement("DELETE FROM users WHERE id = ?")) {
-            st.setLong(1, object.getId());
-            st.executeUpdate();
-        }
+
     }
 
     @Override
     public User merge(User object) throws SQLException {
-        try(PreparedStatement st = connection.prepareStatement("UPDATE users SET email = ?, password = ? WHERE id = ?", Statement.RETURN_GENERATED_KEYS)) {
-            st.setString(1, object.getMail());
-            st.setString(2, object.getPassword());
-            st.setLong(3, object.getId());
-            st.executeUpdate();
-            try(ResultSet rs = st.getGeneratedKeys()) {
-                if(rs.next()) {
-                    object.setId(rs.getLong(1));
-                }
-            }
-            return object;
-        }
+        return null;
     }
 
     @Override
     public User find(long id) throws SQLException {
-        try(PreparedStatement st = connection.prepareStatement("SELECT * FROM users WHERE id = ?")) {
-            st.setLong(1, id);
-            try(ResultSet resultSet = st.executeQuery()) {
-                if(resultSet.next()) {
-                    User user = new User(resultSet.getString(2), resultSet.getString(3));
-                    user.setId(resultSet.getLong(1));
-                    return user;
-                }
-            }
-        }
         return null;
     }
 
     @Override
     public User find(String name) throws SQLException {
-        try(PreparedStatement st = connection.prepareStatement("SELECT * FROM users WHERE email = ?")) {
-            st.setString(1, name);
-            try(ResultSet resultSet = st.executeQuery()) {
-                if(resultSet.next()) {
-                    User user = new User(resultSet.getString(2), resultSet.getString(3));
-                    user.setId(resultSet.getLong(1));
-                    return user;
-                }
-            }
-        }
         return null;
     }
 
     @Override
     public List<User> findAll() throws SQLException {
-        List<User> users = new ArrayList<>();
-        try(PreparedStatement st = connection.prepareStatement("SELECT * FROM users")) {
-            try(ResultSet resultSet = st.executeQuery()) {
-                while (resultSet.next()) {
-                    User user = new User(resultSet.getString(2), resultSet.getString(3));
-                    user.setId(resultSet.getLong(1));
-                    users.add(user);
-                }
-            }
-        }
-        return users;
+        return null;
     }
 }
